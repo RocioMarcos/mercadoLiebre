625f215504b1c4bae43a0c5e45df0b68eb2e0611
Roo <cherryrooma@gmail.com>
2023-11-09 19:02:53 -0300

    [EDIT] forms

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ app.get('/', (req, res) =>{
 });
 
 app.get('/registrar', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, '/views/register.html'));
+    res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
 app.get('/login', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, '/views/login.html'));
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
 
 /* UP */
